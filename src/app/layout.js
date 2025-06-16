168770414b31b74f197ae45b4afc3aa554775cdd
naeemudheen p <nayeemudheenaslam3@gmail.com>
2025-06-16 22:36:58 +0530

    Updated seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,8 @@ import Footer from "@/components/footer";
 export const metadata = {
   title: "Online Lottie Previewer",
   description: "Easily preview and compare Lottie animations in Online across dark and light themes. Perfect for designers and developers to test Lottie JSON files using lottie-react and react-lottie-player.",
+  keywords: ['Lottie Previewer', 'Lottie JSON Viewer', 'lottie-react', 'React Lottie Player', 'Online Lottie Previewer', 'Open Lottie Previewer', 'Themes', 'Dark Mode', 'Light Mode', 'lottie players'],
+  authors: [{ name: 'Naeemudheen P' }],
 };
 
 export default function RootLayout({ children }) {
